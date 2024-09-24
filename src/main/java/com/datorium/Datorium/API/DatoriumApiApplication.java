fbd6343bdee152220a9cc79079895df9bcba75be
Anita Apele <anitaa@Anitas-MacBook-Pro.local>
2024-09-24 18:08:11 +0300

    cleaned up unnecessary lines

--- src/main/java/com/datorium/Datorium/API/DatoriumApiApplication.java
+++ src/main/java/com/datorium/Datorium/API/DatoriumApiApplication.java
@@ -1,13 +1,11 @@
 package com.datorium.Datorium.API;
 
-import com.datorium.Datorium.API.DTOs.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
